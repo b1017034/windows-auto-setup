262295eb4681066f84c2752990d250f7952adb7a
Asahi <b1017034@gmail.com>
2022-11-14 02:39:04 +0900

    Update winget-getapp.ps1

--- winget-getapp.ps1
+++ winget-getapp.ps1
@@ -1,1 +1,13 @@
- winget install 9N0DX20HK701 --accept-source-agreements --accept-package-agreements ## Windows Terminal
+winget install Microsoft.WindowsTerminal --accept-source-agreements --accept-package-agreements
+winget install JetBrains.Toolbox
+winget install Microsoft.VisualStudio.2022.Community
+winget install SlackTechnologies.Slack
+winget install Discord.Discord
+winget install Git.Git
+winget install Google.Chrome
+winget install Mozilla.Firefox
+winget install Docker.DockerDesktop
+winget install TeamViewer.TeamViewer
+winget install NZXT.CAM
+winget install Logitech.GHUB
+winget install Valve.Steam
